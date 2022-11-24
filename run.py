4b72b5a55a5844f1d5cb0bb0d8e7f3fb829a7cac
frapplecat <nigel1138@hotmail.com>
2022-11-24 19:27:47 +0000

    Import random library and word list

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import word_list
+
+def get_word():
+    """
+    Function that returns a word for the hangman game
+    """
+    word = random.choice(word_list)
+    """
+    Picks a random word from word_list
+    """
+    return word.upper()
+    """
+    returns the random word in uppercase
+    """
